f1e46d634c52458700fbf8f42047f16b9655c746
jchwin <jchcamareno@gmail.com>
2023-01-01 09:56:06 -0500

    Avance de curso

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
 	import About from "./components/About.svelte";
 	import Text from "./components/Text.svelte";
 	import Person from "./components/Person.svelte";
+	import Skills from "./components/Skills.svelte";
 	export let name;
 	export let lastname;
 	let svelteLogo = "https://arepa.s3.amazonaws.com/svelte-logo.png"
@@ -18,7 +19,8 @@
 	<About />
 	<Text anotherText="Hola!" />
 	<Text />
-	<Person {...data} />	
+	<Person {...data} />
+	<Skills />	
 	<img src={svelteLogo} alt="svelte" />
 </main>
 
@@ -30,13 +32,15 @@
 
 	:global(:root){
 		--theme-color:purple;
+		--color-dark: #f7f7f7;
 	}	
 
-	:global(body.dark-mode){
-		background-color:#1d3040;
-		color:#bfc2c7;
+	:global(body.dark-mode, span){
+		background-color: #1d3040;
+		color: #bfc2c7;
+		transition: background-color 300ms, color 300ms;
+		--theme-color: #bfc2c7;
 	}
-	
 	main {
 		text-align: center;
 		padding: 1em;
